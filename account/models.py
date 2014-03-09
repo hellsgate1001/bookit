c0beb16132b624dfd1d863e4635cb1a1ced8421c
Brian Wilson <brianwilson1001@hotmail.com>
2014-03-09 15:18:02 +0000

    Add custom user model in account app Update bookit base model to 'soft delete' where applicable

--- account/models.py
+++ account/models.py
@@ -1,26 +1,15 @@
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from bookit.models import BookitModel
 
-class User(AbstractUser):
-    MALE = 'M'
-    FEMALE = 'F'
-    GENDER_CHOICES = (
-        (MALE, 'Male'),
-        (FEMALE, 'Female')
-    )
 
+class UserProfile(AbstractUser):
     middle_names = models.CharField(max_length=256, blank=True, default='')
-    address1 = models.CharField(max_length=255, default='')
-    address2 = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=255, default='')
-    region = models.CharField(max_length=255, blank=True)
-    country = models.CharField(max_length=255, default='')
-    postcode = models.CharField(max_length=10, default='')
-    dob = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
-    telephone = models.CharField(max_length=20, default='')
-    mobile = models.CharField(max_length=20, blank=True)
+
+    deleted_by = models.ForeignKey('self', blank=True, null=True, related_name='+')
+    date_deleted = models.DateTimeField(blank=True, null=True)
 
     def __unicode__(self):
-        return '%(username)s (%(email)s)' % {'username': self.username, 'email', self.email}
+        return ('%(username)s (%(email)s)' %
+            {'username': self.username, 'email', self.email})
--- bookit/models.py
+++ bookit/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BookitModel(models.Model):
     request = None
 
     def delete(self):
-        if hasattr(self, 'active'):
-            self.active = False
+        if hasattr(self, 'is_active'):
+            self.is_active = False
+            if hasattr(self, 'deleted_by'):
+                self.deleted_by = self.request.user
+            if hasattr(self, 'date_deleted'):
+                date_deleted = timezone.datetime.now()
             self.save()
         else:
             super(BookitModel, self).delete()
