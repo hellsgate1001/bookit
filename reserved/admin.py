fa2a809de49e6c9f86ef9682d8572e13a2d26f37
Jay Jagpal <jay@strangemother.com>
2014-06-29 16:28:12 +0100

    mapped baseic model structure

--- reserved/admin.py
+++ reserved/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
-from models import Location, Company
+from models import Location, Customer, TelephoneType, Telephone, Company, Venue, Booking, Event
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('address', 'computed_address', 'latitude', 'longitude', 'geocode_error', 'address_ptr', 'name', )
     list_filter = ('address', 'computed_address', 'latitude', 'longitude', 'geocode_error', 'address_ptr', 'name', )
     search_fields = ('address', 'computed_address', 'latitude', 'longitude', 'geocode_error', 'address_ptr', 'name', )
     #fields = ('address', 'computed_address', 'latitude', 'longitude', 'geocode_error', 'address_ptr', 'name', )
+    filter_horizontal = ()
+    #exclude = (,)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', )
+    list_filter = ('name', 'email', )
+    search_fields = ('name', 'email', )
+    #fields = ('name', 'email', )
+    filter_horizontal = ()
+    #exclude = (,)
+
+class TelephoneTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_filter = ('name', )
+    search_fields = ('name', )
+    #fields = ('name', )
+    filter_horizontal = ()
+    #exclude = (,)
+
+class TelephoneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'number', )
+    list_filter = ('name', 'owner', 'number', )
+    search_fields = ('name', 'owner', 'number', )
+    #fields = ('name', 'owner', 'number', )
     filter_horizontal = ()
     #exclude = (,)
 
@@ -17,8 +41,38 @@ class CompanyAdmin(admin.ModelAdmin):
     filter_horizontal = ()
     #exclude = (,)
 
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'company', )
+    list_filter = ('name', 'address', 'company', )
+    search_fields = ('name', 'address', 'company', )
+    #fields = ('name', 'address', 'company', )
+    filter_horizontal = ()
+    #exclude = (,)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'updated', 'arrival', 'depature', 'company', 'status', )
+    list_filter = ('name', 'created', 'updated', 'arrival', 'depature', 'company', 'status', )
+    search_fields = ('name', 'created', 'updated', 'arrival', 'depature', 'company', 'status', )
+    #fields = ('name', 'created', 'updated', 'arrival', 'depature', 'company', 'status', )
+    filter_horizontal = ()
+    #exclude = (,)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'start_time', 'end_time', )
+    list_filter = ('name', 'company', 'start_time', 'end_time', )
+    search_fields = ('name', 'company', 'start_time', 'end_time', )
+    #fields = ('name', 'company', 'start_time', 'end_time', )
+    filter_horizontal = ()
+    #exclude = (,)
+
 
 
 admin.site.register(Location, LocationAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(TelephoneType, TelephoneTypeAdmin)
+admin.site.register(Telephone, TelephoneAdmin)
 admin.site.register(Company, CompanyAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Event, EventAdmin)
 
--- reserved/models.py
+++ reserved/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from account.models import UserProfile
 from easy_maps.models import Address
 
+from phonenumber_field.modelfields import PhoneNumberField
 
 class BookitModel(models.Model):
     request = None
@@ -21,24 +22,69 @@ class BookitModel(models.Model):
     class Meta:
         abstract = True
 
-
 class Location(Address):
-    '''
-    Define a location for a point on the planet.
-    '''
+    '''Define a location for a point on the planet.'''
     name = models.CharField(max_length=255, help_text='Name of the location')
 
 
+
+class Customer(models.Model):
+    '''
+    A customer defines an entity for puchasing bookings.
+    A  Customer does not directly associate a Telehpone. A valid reference
+    through the inheritence chain as a Telephone needs a Customer - But as
+    Customer does not need a Telephone
+    '''
+    name = models.CharField(max_length=255)
+    address = models.ManyToManyField(Location)
+    email = models.EmailField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "Customer: %s:%s" % (self.name, self.email)
+
+class TelephoneType(models.Model):
+    name = models.CharField(max_length=255, help_text='The name of the contact \
+        phone number. i.e "Work" ')
+
+    def __unicode__(self):
+        return self.name
+
+class Telephone(models.Model):
+    '''
+    The Telephone model assocates a Customer. A Customer can have many
+    telephone numbers.
+
+    The Telephone object has a reference to a Customer. therefore a Customer
+    does not have a direct reference to a Telephone.
+
+    As in; A Telephone must be owned by person, but a person(Customer) does
+    not require a Telephone.
+
+    '''
+    name = models.ForeignKey(TelephoneType)
+    owner = models.ForeignKey(Customer)
+    number = PhoneNumberField()
+
+    def __unicode__(self):
+        return "%s %s: %s" % (self.owner.name, self.name, self.number)
+
+
 class Company(models.Model):
+    ''' A company defines an existing agent, of which has an address and
+    a contact person.'''
+
     name = models.CharField(max_length=255)
     addresses = models.ManyToManyField(Location)
     contact = models.ManyToManyField(UserProfile)
+    telephone = models.ManyToManyField(Telephone)
 
     def __unicode__(self):
         return "Company: %s" % self.name
 
-
 class Venue(models.Model):
+    '''A venue is a location of which events occur. A venue is a location owned
+    by a company, Many Bookings can be made at a Venue through any Event.'''
+
     name = models.CharField(max_length=255,
         help_text='Fiendly name of the venue such as <i>O2 Arena</i> ')
 
@@ -48,9 +94,76 @@ class Venue(models.Model):
     company = models.ForeignKey(Company,
         help_text='Company in charge of the venue')
 
-    contact = models.ManyToManyField(UserProfile,
+    contact = models.ManyToManyField(Customer,
         help_text='People to contact for event coordiation')
 
 
     def __unicode__(self):
         return "Venue: %s by %s" % (self.name, self.company)
+
+
+class Booking(models.Model):
+    ''' A Booking associates an event with a customer. The Customer will
+    attend the Venue at the datetime provided.
+    The status of the booking is goverened externally. '''
+
+    '''
+    If no name is provided, the booking name is the Customer. If a customer
+    has been ommited, The company name is used. Without a Customer or Company,
+    a default booking name is provided.
+    '''
+    name = models.CharField(max_length=255, help_text='Optionally name of the booking entity.')
+
+
+    created = models.DateTimeField(auto_now=False, editable=False,
+        null=True, blank=True, auto_now_add=True,
+        help_text='The Booking creation time.')
+
+    updated = models.DateTimeField(auto_now=True, editable=False,
+        null=True, blank=True, auto_now_add=False,
+        help_text='The Booking update time.')
+
+    arrival = models.DateTimeField(null=True, blank=True, help_text='The expected arrival time of \
+        the customer. If no arrival time is applied, the booking event \
+        default start time will be used.')
+
+    depature = models.DateTimeField(null=True, blank=True, help_text='The expected \
+        leaving time of the customer. If no deature time is applied, the \
+        booking event default end time will be used.')
+
+    company = models.ForeignKey(Company,null=True, blank=True,
+        help_text='Did a company create this booking.')
+
+    customers =  models.ManyToManyField(Customer,null=True, blank=True,
+        help_text='People to contact for event coordiation')
+
+    status = models.CharField(max_length=100,null=True, blank=True)
+
+
+class Event(models.Model):
+    ''' An Event defines a location for many bookings to occur. Each booking
+    associates a customer and a time to an event.
+    An event may occur over many venues (An event may last more than one
+        venue location.)
+    Any Event may have a number of bookings, containing Customers and
+    times. '''
+
+    name = models.CharField(max_length=255,
+        help_text='Fiendly name of the event such as <i>Slipknot 9.0</i> ')
+
+    company = models.ForeignKey(Company, null=True, blank=True,
+        help_text='Company in charge of the event')
+
+    venues = models.ManyToManyField(Venue,
+        help_text='Venues of which this event takes place')
+
+    bookings = models.ManyToManyField(Booking, null=True, blank=True,
+        help_text='Venues of which this event takes place')
+
+    start_time = models.DateTimeField( null=True, blank=True,
+        help_text='The Date and time the Event \
+        open. If no start time is applied, the event is immediately open.')
+
+    end_time = models.DateTimeField( null=True, blank=True,
+        help_text='The Date and time the Event \
+        finish. If no end time is applied, the event is always open.')
